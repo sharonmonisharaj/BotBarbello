9e47b2da36340b83a531e5b207b08e1cef3fdc8f
sharonmonisharaj <sharonmonisharaj@gmail.com>
2016-12-10 16:12:36 -0500

    test

--- app.rb
+++ app.rb
@@ -9,17 +9,12 @@ require 'haml'
 
 # ----------------------------------------------------------------------
 
-# Load environment variables using Dotenv. If a .env file exists, it will
-# set environment variables from that file (useful for dev environments)
 configure :development do
   require 'dotenv'
   Dotenv.load
 end
 
-# require any models 
-# you add to the folder
-# using the following syntax:
-
+# ----------------------------------------------------------------------
 
 require_relative 'app'
 require_relative './models/dumbbell_exercise'
@@ -32,7 +27,6 @@ require_relative './models/tool'
 require_relative './models/workout_type'
 require_relative './models/team'
 require_relative './models/event'
-
 
 # enable sessions for this project
 enable :sessions
@@ -53,30 +47,11 @@ get "/about" do
   "About this app"
 end
 
-
 # ----------------------------------------------------------------------
 
-get "/login/:user" do 
-  
-  #send_slack_request "Good news\n #{params[:user]} just signed into the app." 
-  
+get "/login/:user" do  
   send_slack_request "Good news\n #{params[:user]} just signed into the app.", ["https://github.com/daraghbyrne"]
-  
 end 
-
-# check for token = 2JWqGx57O5oZXPCye5cOX4kz
-
-# Params I'll receive
-# token=2JWqGx57O5oZXPCye5cOX4kz
-# team_id=T0001
-# team_domain=example
-# channel_id=C2147483705
-# channel_name=test
-# user_id=U2147483697
-# user_name=Steve
-# command=/weather
-# text=94070
-# response_url=https://hooks.slack.com/commands/1234/5678
 
 # ----------------------------------------------------------------------
 
@@ -128,14 +103,9 @@ get "/oauth" do
   
 end
 
-# If successful this will give us something like this:
-# {"ok"=>true, "access_token"=>"xoxp-92618588033-92603015268-110199165062-deab8ccb6e1d119caaa1b3f2c3e7d690", "scope"=>"identify,bot,commands,incoming-webhook", "user_id"=>"U2QHR0F7W", "team_name"=>"Programming for Online Prototypes", "team_id"=>"T2QJ6HA0Z", "incoming_webhook"=>{"channel"=>"bot-testing", "channel_id"=>"G36QREX9P", "configuration_url"=>"https://onlineprototypes2016.slack.com/services/B385V4V8E", "url"=>"https://hooks.slack.com/services/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"}, "bot"=>{"bot_user_id"=>"U37HMQRS8", "bot_access_token"=>"xoxb-109599841892-oTaxqITzZ8fUSdmMDxl5kraO"}
-
-
 # ----------------------------------------------------------------------
 #     MONITOR EVENTS
 # ----------------------------------------------------------------------
-
 
 post "/events" do 
   request.body.rewind
@@ -162,6 +132,7 @@ post "/events" do
   
 end
 
+# ----------------------------------------------------------------------
 
 post "/interactive_buttons" do
 
@@ -221,12 +192,7 @@ post "/interactive_buttons" do
     200
     
   end
-
-  
-  
 end 
-
-
 
 # ----------------------------------------------------------------------
 #     SLASH COMMANDS
@@ -278,155 +244,36 @@ post "/workout/"  do
 end
 
 # ----------------------------------------------------------------------
+#     OUTGOING WEBHOOKS
+# ----------------------------------------------------------------------
 
-post "/dumbbell_slash_cmd/" do
-
-  puts params.to_s
-
-  slack_token = "X5Xu7haqc2oBLZLSZBhs8hIS"
-
-  if slack_token == params[:token]
-    
-    channel_name = params[:channel_name]
-    user_name = params[:user_name]
-    text = params[:text]
-    response_url = params[:response_url]
-    
-    greeting = ["Hey dude!", "Hey man!", "Hey brother!", "It's great to see you bro!"]
-    
-    random = DumbbellExercise.all.sample(1).first
-    formatted_message = greeting.sample + " Here's your new dumbbell workout video!\n\n" + Rainbow(random.name).underline + "\n" + random.dumbbell
-
-    content_type :json
-  
-    {text: formatted_message, response_type: "in_channel" }.to_json
-
-  else
-    content_type :json
-    {text: "Invalid Request", response_type: "ephemeral" }.to_json
-
-  end
-
+post "/intro/" do
+   content_type :json {
+      "text": "Hi! I'm BotBarbello. I'm here to empower you with new workout videos everyday! Building a stronger you is my life's only goal.\n\nHere's how you can make the best use of me with slash commands:\n\nSimply type `workout` followed by `dumbbell`, `barbell` or `cardio` into a channel of your choice to quickly get started.", response_type: "in channel" }.to_json
+      "attachments": [
+        {
+            "text": "If you want a more streamlined approach to your workout, start here!",
+            "fallback": "Start your cardio workout by typing `workout cardio`",
+            "callback_id": "wopr_game",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "start_workout",
+                    "text": "Start Workout!",
+                    "style": "danger",
+                    "type": "button",
+                    "value": "start_workout"
+                }
+            ]
+        }
+    ]
+}.to_json
 end
 
 # ----------------------------------------------------------------------
 
-post "/barbell_slash_cmd/" do
-
-  puts params.to_s
-
-  slack_token = "4hJ7uwXro4ChP43cUV6LJc1W"
-
-  if slack_token == params[:token]
-    
-    channel_name = params[:channel_name]
-    user_name = params[:user_name]
-    text = params[:text]
-    response_url = params[:response_url]
-
-    
-    greeting = ["Hey dude!", "Hey man!", "Hey brother!", "It's great to see you bro!"]
-    
-    random = BarbellExercise.all.sample(1).first
-    formatted_message = greeting.sample + " Here's your new barbell workout video!\n\n" + Rainbow(random.name).underline + "\n" + random.barbell
-
-    content_type :json
-  
-    {text: formatted_message, response_type: "in_channel" }.to_json
-
-  else
-    content_type :json
-    {text: "Invalid Request", response_type: "ephemeral" }.to_json
-
-  end
-
-end
-
-# ----------------------------------------------------------------------
-
-post "/cardio_slash_cmd/" do
-
-  puts params.to_s
-
-  slack_token = "QRguIojaAvbSaXaK5IYFcuCC"
-
-  if slack_token == params[:token]
-    
-    channel_name = params[:channel_name]
-    user_name = params[:user_name]
-    text = params[:text]
-    response_url = params[:response_url]
-
-    
-    greeting = ["Hey dude!", "Hey man!", "Hey brother!", "It's great to see you bro!"]
-    
-    random = CardioExercise.all.sample(1).first
-    formatted_message = greeting.sample + " Here's your new cardio workout video!\n\n" + Rainbow(random.name).underline + "\n" + random.cardio
-
-    content_type :json
-  
-    {text: formatted_message, response_type: "in_channel" }.to_json
-
-  else
-    content_type :json
-    {text: "Invalid Request", response_type: "ephemeral" }.to_json
-
-  end
-
-end
-
-# ----------------------------------------------------------------------
-#     OUTGOING WEBHOOK
-# ----------------------------------------------------------------------
-
-# post "/botbarbello_intro/" do
-#    content_type :json
-#    {
-#        "text": "Would you like to play a game?",
-#        "attachments": [
-#            {
-#                "text": "Choose a game to play",
-#                "fallback": "You are unable to choose a game",
-#                "callback_id": "wopr_game",
-#                "color": "#3AA3E3",
-#                "attachment_type": "default",
-#                "actions": [
-#                    {
-#                        "name": "chess",
-#                        "text": "Chess",
-#                        "type": "button",
-#                        "value": "chess"
-#                    },
-#                    {
-#                        "name": "maze",
-#                        "text": "Falken's Maze",
-#                        "type": "button",
-#                        "value": "maze"
-#                    },
-#                    {
-#                        "name": "war",
-#                        "text": "Thermonuclear War",
-#                        "style": "danger",
-#                        "type": "button",
-#                        "value": "war",
-#                        "confirm": {
-#                            "title": "Are you sure?",
-#                            "text": "Wouldn't you prefer a good game of chess?",
-#                            "ok_text": "Yes",
-#                            "dismiss_text": "No"
-#                        }
-#                    }
-#                ]
-#            }
-#        ]
-#    }end
-#
-# # ----------------------------------------------------------------------
-
-post "/outgoing/" do
-#    content_type :json
-#    {text: "Hi! I'm BotBarbello. I'm here to empower you with a new high energy body-building workout video everyday! Building a stronger you is my life's only goal.\n\nHere's how you can make the best use of me with slash commands:\n\nSimply type any of the following into a channel of your choice:\n\n/dumbbell\n/barbell\n/cardio\n/abs\n\nFor help, simply type the word 'help'.", response_type: "in channel" }.to_json
-# end
+post "/step_one/" do
 
 content_type :json
 {
@@ -727,24 +574,10 @@ end
 
 # ----------------------------------------------------------------------
 #   METHODS
-#   Add any custom methods below
 # ----------------------------------------------------------------------
 
 private
 
-
-#"https://hooks.slack.com/services/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"
-#payload
-
-# You have two options for sending data to the Webhook URL above:
-# Send a JSON string as the payload parameter in a POST request
-# Send a JSON string as the body of a POST request
-# For a simple message, your JSON payload could contain a text property at minimum. This is the text that will be posted to the channel.
-# A simple example:
-# payload={"text": "This is a line of text in a channel.\nAnd this is another line of text."}
-
-
-# for example 
 def send_slack_request message
 
   slack_webhook = "https://hooks.slack.com/services/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"
@@ -755,45 +588,8 @@ def send_slack_request message
   
 end
 
-#
-# def send_slack_request message, links
-#
-#   slack_webhook = "https://hooks.slack.com/services/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"
-#
-#   formatted_message = message.to_s + "\n"
-#   links.each do |link|
-#     formatted_message += "<#{link.to_s}>".to_s
-#   end
-#
-#   HTTParty.post slack_webhook, body: {text: formatted_message.to_s, username: "AppBot", channel: "bots"}.to_json, headers: {'content-type' => 'application/json'}
-#
-#   response
-#
-# end
+# ----------------------------------------------------------------------  
 
-
-# -----------------------------------------------------------------
-#     TO DO
-# -----------------------------------------------------------------
-
-# Connect to YouTube API to retrieve workout video URLs based on keywords entered
-
-# Connect to Slack API to  to improve efficiency of outgoing webhooks 
-
-# Add custom integrations to a slack app
-
-# Add outgoing webhooks to retrieve specific workout videos from the database
-
-# Restrict to certain YouTube Channel
-
-# Male or female? settings
-
-# Microformating in slack
-
-
-
-
-# for example 
 def respond_to_slack_event json
   
   # find the team 
@@ -876,9 +672,5 @@ def respond_to_slack_event json
   elsif event.formatted_text.include? "help"
     client.chat_postMessage(channel: event.channel, text: "help text", as_user: true)
 
-  
-
   end 
-  
-  
 end
